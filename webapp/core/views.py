d681cb12a3a31f44d67d4ec5f6a1218df33537d9
Javier Cordero <jcorderomartinez@gmail.com>
2014-04-25 11:51:45 +0200

    Issue #7 show static pages in landing page

--- webapp/core/views.py
+++ webapp/core/views.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from django.http                    import Http404
-from django.shortcuts               import render_to_response
+from django.shortcuts               import render_to_response, get_object_or_404
 from django.template                import RequestContext, TemplateDoesNotExist
 from django.views.decorators.csrf   import ensure_csrf_cookie
+from .models import Page
 
 @ensure_csrf_cookie
-def home(request):    
+def home(request):
     # Render template without any argument
-    return render_to_response('index.html', context_instance=RequestContext(request))
+    pages = Page.objects.all().values('title', 'slug')
+    return render_to_response(
+        'index.html', {'pages': pages }, context_instance=RequestContext(request))
 
-def partial(request, partial_name=None):    
+def partial(request, partial_name=None):
     template_name = 'partials/' + partial_name + '.html';
     try:
         return render_to_response(template_name, context_instance=RequestContext(request))
@@ -19,3 +22,9 @@ def partial(request, partial_name=None):
 
 def embed(request):
     return render_to_response('embed.html', context_instance=RequestContext(request))
+
+
+def pages(request, slug):
+    page = get_object_or_404(Page, slug__iexact=slug)
+    return render_to_response(
+        'static_page.html', {'page': page }, context_instance=RequestContext(request))
